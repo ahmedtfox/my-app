872d0baf0b89ce4cd5c695387cb55dc94007f0c1
AhmedMahfouz <ahmedmahfouzt@gmail.com>
2024-10-14 23:09:21 +0300

    session 1

---
+++ src/Greeting.js
@@ -0,0 +1,13 @@
+function Greeting() {
+  return (
+    <div className="Greeting">
+      <h5>
+        You can, however, create more top-level directories. They will not be
+        included in the production build so you can use them for things like
+        documentation.
+      </h5>
+    </div>
+  );
+}
+
+export default Greeting;
