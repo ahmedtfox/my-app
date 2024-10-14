143b8d9e13b0a43f0d994ca8de29d456dd22c47c
AhmedMahfouz <ahmedmahfouzt@gmail.com>
2024-10-15 01:07:10 +0300

    setState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Greeting from "./components/Greeting";
 import Welcome from "./components/Welcome";
+import Button from "./components/Button";
+
 function App() {
   return (
     <div className="App">
       <h1>welcome </h1>
       <Greeting name="Sara" />
+
       <Welcome name="ahmed" />
     </div>
   );
--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,34 @@
 import React from "react";
-
+import Button from "./Button";
 class Greeting extends React.Component {
+  state = {
+    name: "ahmed",
+    age: 28,
+  };
   render() {
-    return <h1> Hello {this.props.name}</h1>;
+    return (
+      <>
+        <h1> Hellooooo {this.state.name}</h1>
+        <h3> {this.state.age}</h3>
+        <button
+          onClick={() => {
+            console.log(this.state.name);
+            this.setState({
+              name: "kkkk",
+            });
+          }}
+        >
+          change
+        </button>
+        <Button />
+      </>
+    );
+  }
+
+  componentDidMount() {
+    this.setState({
+      age: 29,
+    });
   }
 }
 
