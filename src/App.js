fb36ec9d4e45f9f66d119ad4585cb6878c5d7c11
AhmedMahfouz <ahmedmahfouzt@gmail.com>
2024-10-16 02:12:50 +0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import productData from "./components/productData";
 import Product from "./components/Product";
+import Count from "./components/Count";
 function App() {
   const list = productData.map((p) => {
     return p.price > 50 ? <Product key={p.id} product={p} /> : null;
   });
   return (
     <div className="App">
-      <div className="row">{list}</div>
+      <Count />
     </div>
   );
 }
