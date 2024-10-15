5eaf77a295ca89d00834fa001942c2b464dc34c5
AhmedMahfouz <ahmedmahfouzt@gmail.com>
2024-10-16 00:42:25 +0300

    store example

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import Greeting from "./components/Greeting";
-import Welcome from "./components/Welcome";
-import Button from "./components/Button";
-import Form from "./components/Form";
-import List from "./components/List";
+import productData from "./components/productData";
+import Product from "./components/Product";
 function App() {
+  const list = productData.map((p) => {
+    return p.price > 50 ? <Product key={p.id} product={p} /> : null;
+  });
   return (
     <div className="App">
-      <Form />
-      <br />
-      <List />
+      <div className="row">{list}</div>
     </div>
   );
 }
